9b2149f2dfe37e4633752cfb3567ea0602d1f141
artem <zhiruyev_a@bankrkb.kz>
2023-03-21 22:34:47 +0600

    models to admin added

--- exams/admin.py
+++ exams/admin.py
@@ -1,3 +1,158 @@
 from django.contrib import admin
+from .models import (
+    School,
+    Subject,
+    Exam,
+    OrdinaryQuestion,
+    OrdinaryQuestionAnswer,
+    StudentExam,
+    OrdinaryQuestionUserAnswer,
+    ComparisonQuestion,
+    ComparisonQuestionOption,
+    ComparisonQuestionOptionAnswer,
+    ComparisonQuestionUserAnswer,
+)
 
-# Register your models here.
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+    )
+
+    search_fields = (
+        "name",
+    )
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "school",
+    )
+
+    list_filter = (
+        "school",
+    )
+
+    search_fields = (
+        "name",
+    )
+
+
+@admin.register(Exam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "subject",
+    )
+
+    list_filter = (
+        "subject",
+    )
+
+    search_fields = (
+        "name",
+    )
+
+
+@admin.register(OrdinaryQuestion)
+class OrdinaryQuestionAdmin(admin.ModelAdmin):
+    list_display = (
+        "header",
+    )
+
+    list_filter = (
+        "exam",
+    )
+
+    search_fields = (
+        "header",
+    )
+
+
+@admin.register(OrdinaryQuestionAnswer)
+class OrdinaryQuestionAnswerAdmin(admin.ModelAdmin):
+    list_display = (
+        "question",
+    )
+
+    list_filter = (
+        "is_correct",
+    )
+
+    search_fields = (
+        "question",
+    )
+
+
+@admin.register(StudentExam)
+class StudentExamAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "exam",
+    )
+
+    list_filter = (
+        "user",
+        "exam",
+    )
+
+
+@admin.register(OrdinaryQuestionUserAnswer)
+class OrdinaryQuestionUserAnswerAdmin(admin.ModelAdmin):
+    list_display = (
+        "question",
+        "answer",
+    )
+
+
+@admin.register(ComparisonQuestion)
+class ComparisonQuestionAdmin(admin.ModelAdmin):
+    list_display = (
+        "header",
+    )
+
+    list_filter = (
+        "exam",
+    )
+
+    search_fields = (
+        "header",
+    )
+
+
+@admin.register(ComparisonQuestionOption)
+class ComparisonQuestionOptionAdmin(admin.ModelAdmin):
+    list_display = (
+        "question",
+    )
+
+    list_filter = (
+        "question",
+    )
+
+
+@admin.register(ComparisonQuestionOptionAnswer)
+class ComparisonQuestionOptionAnswerAdmin(admin.ModelAdmin):
+    list_display = (
+        "option",
+    )
+
+    list_filter = (
+        "option",
+    )
+
+
+@admin.register(ComparisonQuestionUserAnswer)
+class ComparisonQuestionUserAnswerAdmin(admin.ModelAdmin):
+    list_display = (
+        "option",
+        "option_answer",
+    )
+
+    list_filter = (
+        "option",
+        "option_answer",
+    )
