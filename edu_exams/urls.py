dece421d83ef8ac3143f5739dec5c69a99a4c031
artem <zhiruyev_a@bankrkb.kz>
2023-03-21 19:25:53 +0600

    libs added

--- edu_exams/urls.py
+++ edu_exams/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(settings.URL_PREFFIX+"admin/", admin.site.urls),
+    path(settings.URL_PREFFIX + "auth/", include("_auth.urls", namespace="auth")),
+    path(settings.URL_PREFFIX + "token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path(settings.URL_PREFFIX + "token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
