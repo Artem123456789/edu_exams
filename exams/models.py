7637d76d646794894b1502d6d8783596a9b488a7
artem <zhiruyev_a@bankrkb.kz>
2023-03-21 22:41:48 +0600

    added meta to models

--- exams/models.py
+++ exams/models.py
@@ -2,6 +2,7 @@ import uuid
 from django.db import models
 from django.contrib.auth import get_user_model
 from django_extensions.db.models import TimeStampedModel
+from django.utils.translation import gettext as _
 
 from libs.base_models import (
     NamedModel,
@@ -17,6 +18,10 @@ class School(NamedModel):
     """
     uuid = models.UUIDField(default=uuid.uuid4, primary_key=True)
 
+    class Meta:
+        verbose_name = _("Образовательное учреждение")
+        verbose_name_plural = _("Образовательные учреждения")
+
     def __str__(self):
         return self.name
 
@@ -27,6 +32,10 @@ class Subject(NamedModel):
     """
     uuid = models.UUIDField(default=uuid.uuid4, primary_key=True)
     school = models.ForeignKey(School, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = _("Дисциплина")
+        verbose_name_plural = _("Дисциплины")
 
     def __str__(self):
         return f"{self.name} {self.school.name}"
@@ -39,6 +48,10 @@ class Exam(NamedModel):
     uuid = models.UUIDField(default=uuid.uuid4, primary_key=True)
     subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True)
 
+    class Meta:
+        verbose_name = _("Экзамен")
+        verbose_name_plural = _("Экзамены")
+
     def __str__(self):
         return f"{self.name} {self.subject.name}"
 
@@ -50,6 +63,10 @@ class OrdinaryQuestion(QuestionModel):
     uuid = models.UUIDField(default=uuid.uuid4, primary_key=True)
     exam = models.ForeignKey(Exam, on_delete=models.SET_NULL, null=True)
 
+    class Meta:
+        verbose_name = _("Вопрос с одним вариантом ответа")
+        verbose_name_plural = _("Вопросы с одним вариантом ответа")
+
 
 class OrdinaryQuestionAnswer(models.Model):
     """
@@ -58,6 +75,10 @@ class OrdinaryQuestionAnswer(models.Model):
     question = models.ForeignKey(OrdinaryQuestion, on_delete=models.SET_NULL, null=True)
     text = models.TextField()
     is_correct = models.BooleanField()
+
+    class Meta:
+        verbose_name = _("Ответ на вопрос с одним вариантом ответа")
+        verbose_name_plural = _("Ответы на вопрос с одним вариантом ответа")
 
     def __str__(self):
         return self.text[:20]
@@ -70,6 +91,10 @@ class StudentExam(TimeStampedModel):
     uuid = models.UUIDField(default=uuid.uuid4, primary_key=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     exam = models.ForeignKey(Exam, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = _("Экзамен студента")
+        verbose_name_plural = _("Экзамены студента")
 
     def __str__(self):
         return f"{self.user.name} {self.exam}"
@@ -84,6 +109,10 @@ class OrdinaryQuestionUserAnswer(TimeStampedModel):
     question = models.ForeignKey(OrdinaryQuestion, on_delete=models.SET_NULL, null=True)
     answer = models.ForeignKey(OrdinaryQuestionAnswer, on_delete=models.SET_NULL, null=True)
 
+    class Meta:
+        verbose_name = _("Ответ пользователя на вопрос с одним вариантом ответа")
+        verbose_name_plural = _("Ответы пользователя на вопрос с одним вариантом ответа")
+
     def __str__(self):
         return f"{str(self.question)} {str(self.answer)}"
 
@@ -95,6 +124,10 @@ class ComparisonQuestion(QuestionModel):
     uuid = models.UUIDField(default=uuid.uuid4, primary_key=True)
     exam = models.ForeignKey(Exam, on_delete=models.SET_NULL, null=True)
 
+    class Meta:
+        verbose_name = _("Вопрос с сопоставлением ответов")
+        verbose_name_plural = _("Вопросы с сопоставлением ответов")
+
 
 class ComparisonQuestionOption(models.Model):
     """
@@ -103,6 +136,10 @@ class ComparisonQuestionOption(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, primary_key=True)
     question = models.ForeignKey(ComparisonQuestion, on_delete=models.SET_NULL, null=True)
     text = models.TextField()
+
+    class Meta:
+        verbose_name = _("Опция вопроса с сопоставлением ответа")
+        verbose_name_plural = _("Опции вопроса с сопоставлением ответа")
 
     def __str__(self):
         return self.text[:20]
@@ -116,6 +153,10 @@ class ComparisonQuestionOptionAnswer(models.Model):
     text = models.TextField()
     option = models.OneToOneField(ComparisonQuestionOption, on_delete=models.SET_NULL, null=True)
 
+    class Meta:
+        verbose_name = _("Ответ на опцию вопроса с сопоставлением")
+        verbose_name_plural = _("Ответы на опцию вопроса с сопоставлением")
+
     def __str__(self):
         return self.text[:20]
 
@@ -128,6 +169,10 @@ class ComparisonQuestionUserAnswer(TimeStampedModel):
     student_exam = models.ForeignKey(StudentExam, on_delete=models.SET_NULL, null=True)
     option = models.ForeignKey(ComparisonQuestionOption, on_delete=models.SET_NULL, null=True)
     option_answer = models.ForeignKey(ComparisonQuestionOptionAnswer, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = _("Ответ пользователя на вопрос с сопоставлением ответов")
+        verbose_name_plural = _("Ответы пользователя на вопрос с сопоставлением ответов")
 
     def __str__(self):
         return f"{str(self.option)} {str(self.option_answer)}"
