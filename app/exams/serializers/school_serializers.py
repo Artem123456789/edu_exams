211f0f95d7692fffffa3884d6335b7ee7f26b63e
artem <artem@mai.ru>
2023-05-25 17:30:25 +0600

    exams count per school

--- app/exams/serializers/school_serializers.py
+++ app/exams/serializers/school_serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
 
-from app.exams.models import School
+from app.exams.models import (
+    School,
+    Exam,
+)
 
 
 class SchoolListSerializer(serializers.ModelSerializer):
+    exams_count = serializers.SerializerMethodField()
+
+    def get_exams_count(self, obj: School):
+        return Exam.objects.filter(subject__school=obj).count()
 
     class Meta:
         model = School
         fields = [
             'name',
+            'exams_count',
         ]
