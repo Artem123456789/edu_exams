aec5578ef6d5e1b3ad7310ca33a1fb064d90b3e3
artem <artem@mai.ru>
2023-05-25 17:40:00 +0600

    performance rate

--- app/exams/serializers/school_serializers.py
+++ app/exams/serializers/school_serializers.py
@@ -1,20 +1,33 @@
 from rest_framework import serializers
 
+from app.exams.handlers.exams_handlers import StudentExamsHandler
 from app.exams.models import (
     School,
     Exam,
+    StudentExam,
 )
 
 
 class SchoolListSerializer(serializers.ModelSerializer):
     exams_count = serializers.SerializerMethodField()
+    performance_rate = serializers.SerializerMethodField()
 
     def get_exams_count(self, obj: School):
         return Exam.objects.filter(subject__school=obj).count()
+
+    def get_performance_rate(self, obj: School):
+        points = 0
+        school_exams = Exam.objects.filter(subject__school=obj)
+        school_student_exams = StudentExam.objects.filter(exam__in=school_exams)
+        for student_exam in school_student_exams:
+            points += StudentExamsHandler().get_student_exam_results(student_exam).points
+
+        return points
 
     class Meta:
         model = School
         fields = [
             'name',
             'exams_count',
+            'performance_rate'
         ]
